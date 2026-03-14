52d033f45af74dde9ca5f25a22548e80e1e19a73
dragoshare <dragoshare@gmail.com>
2026-03-14 10:52:02 +0100

    Add gitignore

---
+++ StudioCRM.Domain/Entities/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudioCRM.Domain.Entities;
+
+public class Client
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ StudioCRM.Infrastructure/Persistence/StudioCRMDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudioCRM.Domain.Entities;
+
+namespace StudioCRM.Infrastructure.Persistence;
+
+public class StudioCRMDbContext : DbContext
+{
+    public StudioCRMDbContext(DbContextOptions<StudioCRMDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Client> Clients => Set<Client>();
+}
