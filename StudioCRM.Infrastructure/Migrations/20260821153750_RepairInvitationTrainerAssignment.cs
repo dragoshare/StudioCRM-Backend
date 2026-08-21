4d2845b0605033f4b3ab013ccdde58a1033dafd5
dragoshare <dragoshare@gmail.com>
2026-08-21 17:39:33 +0200

    Add invitation trainer schema repair migration

---
+++ StudioCRM.Infrastructure/Migrations/20260821153750_RepairInvitationTrainerAssignment.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudioCRM.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RepairInvitationTrainerAssignment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                ALTER TABLE "Invitations"
+                ADD COLUMN IF NOT EXISTS "TrainerId" integer;
+
+                CREATE INDEX IF NOT EXISTS "IX_Invitations_TrainerId"
+                ON "Invitations" ("TrainerId");
+
+                DO $$
+                BEGIN
+                    IF NOT EXISTS (
+                        SELECT 1
+                        FROM pg_constraint
+                        WHERE conname = 'FK_Invitations_Trainers_TrainerId'
+                            AND conrelid = '"Invitations"'::regclass
+                    ) THEN
+                        ALTER TABLE "Invitations"
+                        ADD CONSTRAINT "FK_Invitations_Trainers_TrainerId"
+                        FOREIGN KEY ("TrainerId")
+                        REFERENCES "Trainers" ("Id")
+                        ON DELETE SET NULL;
+                    END IF;
+                END $$;
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                ALTER TABLE "Invitations"
+                DROP CONSTRAINT IF EXISTS "FK_Invitations_Trainers_TrainerId";
+
+                DROP INDEX IF EXISTS "IX_Invitations_TrainerId";
+
+                ALTER TABLE "Invitations"
+                DROP COLUMN IF EXISTS "TrainerId";
+                """);
+        }
+    }
+}
